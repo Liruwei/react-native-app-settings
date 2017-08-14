375f1a4cc571baed2e4404f2ce1fad52ce0d6824
RuweiLi <liruwei0109@outlook.com>
2017-08-14 14:52:36 +0800

    fix android在主线程执行Intent

--- android/src/main/java/com/krazylabs/OpenAppSettingsModule.java
+++ android/src/main/java/com/krazylabs/OpenAppSettingsModule.java
@@ -24,11 +24,16 @@ public class OpenAppSettingsModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void open() {
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        Uri uri = Uri.fromParts("package", reactContext.getPackageName(), null);
-        intent.setData(uri);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        reactContext.startActivity(intent);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", reactContext.getPackageName(), null);
+                intent.setData(uri);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                reactContext.startActivity(intent);
+            }
+        }).start();
     }
 }
